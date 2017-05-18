f5ba1f35798c79604cd8914451ced54908e90e98
Jesse Verhoeff <jesseenyestin.jv@gmail.com>
2017-05-18 14:37:46 +0200

    workable work space added

--- vendor/bcosca/fatfree/controllers/Controller.php
+++ vendor/bcosca/fatfree/controllers/Controller.php
@@ -3,10 +3,20 @@
 class Controller {
 
 	function beforeroute(){
-		echo 'Before routing - ';
+		?>
+		<div  class="col-md-12">
+		<ul class="nav nav-tabs">
+			<li role="presentation" class="active"><a href="#">Home</a></li>
+			<li role="presentation"><a href="#">Profile</a></li>
+			<li role="presentation"><a href="#">Messages</a></li>
+		</ul>
+
+		<?php
 	}
 
 	function afterroute(){
-		echo '- After routing';
+		?>
+		</div>
+		<?php
 	}
 }
